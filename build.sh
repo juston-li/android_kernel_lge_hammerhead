5a001b52ee904833cceda3dcba8c08b3a8665ab4
Juston Li <juston.h.li@gmail.com>
2014-09-18 01:51:06 -0700

    build script changes
    don't use CONFIG_DEBUG_SECTION_MISMATCH=y
    change final boot.img directory
    
    Change-Id: I6a150f45cfbd240738b1d50a143f0d0b5188f5c5

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-VER="1"
-LOCALVER="-$1"
+RELEASEVER="1"
+TESTVER="-$1"
 
 export PATH=~/bin:$PATH
-export KBUILD_BUILD_VERSION=$VER
-export LOCALVERSION=$LOCALVER
-#export CROSS_COMPILE=/home/juston/cm/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6/bin/arm-linux-androideabi-
-#export CROSS_COMPILE=/home/juston/cm/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
+export KBUILD_BUILD_VERSION=$RELEASEVER
 export CROSS_COMPILE=/home/juston/cm/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
-#export CROSS_COMPILE=/home/juston/cm/prebuilts/gcc/linux-x86/arm/linaro-4.7/bin/arm-linux-androideabi-
-#export CROSS_COMPILE=/home/juston/cm/prebuilts/gcc/linux-x86/arm/gcc-linaro-arm-linux-gnueabihf-4.8-2013.05_linux/bin/arm-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=juston
@@ -24,20 +19,20 @@ make "cyanogenmod_hammerhead_defconfig"
 
 KERNEL_DIR=`pwd`
 
-echo "RELEASEVERSION="KBUILD_BUILD_VERSION
-echo "LOCALVERSION="$LOCALVERSION
+echo "RELEASEVERSION="$KBUILD_BUILD_VERSION
+echo "TESTVERSION="$TESTVER
 echo "CROSS_COMPILE="$CROSS_COMPILE
 echo "ARCH="$ARCH
 echo "KERNEL_DIR="$KERNEL_DIR
 
-#make -j16
-make -j16 CONFIG_DEBUG_SECTION_MISMATCH=y
+make -j16
+#make -j16 CONFIG_DEBUG_SECTION_MISMATCH=y
 
 echo "making bootimg"
 cp arch/arm/boot/zImage-dtb ~/Workspace/mkbootimg
 cd ~/Workspace/mkbootimg
-./mkbootimg --kernel zImage-dtb --ramdisk ramdisk.gz --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 -o boot-t"$VER""$LOCALVER".img
-mv boot-t"$VER""$LOCALVER".img ~/HDD/Dropbox/Kernel
+./mkbootimg --kernel zImage-dtb --ramdisk ramdisk.gz --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 -o boot-t"$RELEASEVER""$TESTVER".img
+mv boot-t"$RELEASEVER""$TESTVER".img ~/Workspace/hammerhead_kernels
 rm zImage-dtb
 
 DATE_END=$(date +"%s")
